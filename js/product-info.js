fbde7e13f371b021bee68b42f7274146c28c882c
larreborges <andresotero2@gmail.com>
2022-08-14 19:13:32 -0300

    Sae

--- js/product-info.js
+++
@@ -1,9 +0,0 @@
-async function datos() {
-    const response = await fetch('https://japceibal.github.io/emercado-api/cats_products/101.json');
-    const json = await response.text();
-
-    console.log(JSON.parse(json));
-    
-}
-
-datos();
--- js/products.js
+++ js/products.js
@@ -1,11 +1,11 @@
-    function loadJSON() {
-        fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
-    .then(function (response){
-        return response.json();
-    })
-    .then(function(data){
-console.log(data);
-
+fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+        document.getElementById("p1").innerHTML = "hola";
+        data.forEach(element => console.log(element));
 })
 
-}
+
+document.getElementById("p2").innerHTML = "pepito"
+
