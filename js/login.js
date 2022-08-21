3cd9398da0370f641d92427cef504797e535b31b
larreborges <andresotero2@gmail.com>
2022-08-21 01:48:06 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,29 @@
+let contra = document.getElementById("floatingPassword");
+
+let nombre = document.getElementById("floatingInput");
+
+function verificarContra() {
+   if (contra.lenght<7 || contra.lenght>40) {
+    alert("Contraseña Inválida")
+   }
+   else{
+    true
+   }
+}
+
+function verificarUsuario() {
+    if (nombre.lenght<7 || nombre.lenght>40) {
+        alert("Usuario Inválido")
+       }
+       else{
+        true
+       }
+}
+
 document.getElementById("btn").onclick = 
     function pageRedirect() {
+        if (verificarContra() && verificarUsuario()){
         window.location.replace("index.html");
     }   
+}
+
