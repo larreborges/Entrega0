821a1311c264e56f60ba17099d6f3b9bf47399ed
larreborges <andresotero2@gmail.com>
2022-08-21 20:44:29 -0300

    Ultimo

--- js/login.js
+++ js/login.js
@@ -1,29 +1,37 @@
-let contra = document.getElementById("floatingPassword");
-
-let nombre = document.getElementById("floatingInput");
-
-function verificarContra() {
-   if (contra.lenght<7 || contra.lenght>40) {
-    alert("Contraseña Inválida")
-   }
-   else{
-    true
-   }
+function verifyPassword() {
+  const pw = document.getElementById("floatingPassword").value;
+  console.log(pw);
+  debugger
+  //si le saco el debugger deja de funcionar correctamente la página, he intentado con muchas formas y esta es la que me funcionó mejor
+  if (pw == "") {
+    alert("Por favor, llene la contraseña");
+    return false;
+  }
+  if (pw.length < 8) {
+    alert("La contraseña debe tener al menos 8 caracteres");
+    return false;
+  }
+  if (pw.length > 30) {
+    alert("La contraseña debe tener como máximo 30 caracteres");
+    return false;
+  }
+  return true;
 }
 
-function verificarUsuario() {
-    if (nombre.lenght<7 || nombre.lenght>40) {
-        alert("Usuario Inválido")
-       }
-       else{
-        true
-       }
+function verifyUsername() {
+  const un = document.getElementById("floatingInput").value;
+
+  if (un == "") {
+    alert("Por favor, complete el email");
+    return false;
+  } else {
+    return true;
+  }
 }
 
-document.getElementById("btn").onclick = 
-    function pageRedirect() {
-        if (verificarContra() && verificarUsuario()){
-        window.location.replace("index.html");
-    }   
-}
-
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+  if (verifyPassword() && verifyUsername()) {
+    window.location.replace("index.html");
+  }
+});
