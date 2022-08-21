bc94661dd9135f02c893ea6ab104ceb704f24895
larreborges <andresotero2@gmail.com>
2022-08-20 23:56:01 -0300

    Falta css

--- js/products.js
+++ js/products.js
@@ -7,22 +7,36 @@ const createCarCard = (carObject) => {
     carCard.setAttribute('id', carObject.id);
 
     const title = document.createElement('h3');
-    title.append(carObject.name)
+    title.append(carObject.name + " - USD " + carObject.cost)
 
+    const description = document.createElement('p');
+    description.append(carObject.description);
+    description.classList.add('description-class');
+
+    const sold = document.createElement('p');
+    sold.append(carObject.soldCount + " vendidos");
+    sold.classList.add('sold-class');
+
+    const image = document.createElement('img');
+    image.src = carObject.image
+    image.classList.add('imagen-class');
+
+    carCard.appendChild(image);
     carCard.appendChild(title);
-    
+    carCard.appendChild(description);
+    carCard.appendChild(sold);
+
+
+
     return carCard;
 }
 
 const populateCarsList = async () => {
     const carData = await getCarData;
 
-    if (carData instanceof Error) {
-        console.error('Unable to load cars');
-    } else if (carData.products) {
+if (carData.products) {
         const carListContainer = document.getElementById('cars-list-container');
         const cars = carData.products;
-    
         cars.forEach(function(carData) {
             carListContainer.appendChild(createCarCard(carData));
         })
@@ -30,3 +44,4 @@ const populateCarsList = async () => {
 }
 
 populateCarsList();
+
