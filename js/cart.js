dbc722ad929c11952e9cd3f8c948a02588b46f4d
larreborges <andresotero2@gmail.com>
2022-10-05 23:28:53 -0300

    Cart

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,29 @@ const getUserData = fetch('https://japceibal.github.io/emercado-api/user_cart/25
 
 const showUserData = async () => {
     const userData = await getUserData;
-    let htmlContentToAppend = "hola";
+    let articulos = userData.articles[0]
+    let nombre = articulos.name;
+    let costo = articulos.unitCost;
+    let cantidad = articulos.count;
+    let moneda = articulos.currency;
+    let imagen = articulos.image;
+
+    let htmlContentToAppend = `
+    
+    <h4>Artículos a comprar</h4>
+      <ul>
+        <li><b>Nombre</b></li>
+        <li>${nombre}</li>
+        <li><b>Costo</b></li>
+        <li>${moneda+costo}</li>
+        <li><b>Cantidad</b></li>
+        <li>${cantidad}</li>
+        <li><b>Subtotal</b></li>
+        <li>${moneda+cantidad*costo}</li>
+        <img src="${imagen}">
+      </ul>
+
+    `
 
     document.getElementById("articulos").innerHTML =
       htmlContentToAppend;
