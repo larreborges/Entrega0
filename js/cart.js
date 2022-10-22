c11b4e363f35efcc85977524bf02fddcebb0d52a
larreborges <andresotero2@gmail.com>
2022-10-22 19:35:19 -0300

    Cart Rows

--- js/cart.js
+++ js/cart.js
@@ -15,11 +15,12 @@ function renderizarCarrito() {
     valueInDollars.toLocaleString("en-US", { minimumFractionDigits: 2 });
 
   let htmlContentToAppend = `
-    
+    <div class="scroller">
     <h4>Artículos a comprar</h4>
       <div class="container">
         <div class="row">
           <div class="col">
+            
           </div>
           <div class="col">
             <li><b>Nombre</b></li>
@@ -34,7 +35,9 @@ function renderizarCarrito() {
             <li><b>Subtotal</b></li>
           </div>
         </div>
+
         <hr/>
+
         <div class="row">
           <div class="col">
             <img src="${image}" width="100">
@@ -58,14 +61,14 @@ function renderizarCarrito() {
       <hr/>
 
       <h4>Tipo de envío</h4>
-        <input type="radio" id="html" name="fav_language" value="HTML">
-          <label for="html">Premium 2 a 5 días (15%)</label><br>
-        <input type="radio" id="css" name="fav_language" value="CSS">
-          <label for="css">Express 5 a 8 días (7%)</label><br>
-        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-          <label for="javascript">Standard 12 a 15 días (5%)</label>
-          <br>
-          <br>
+        <input type="radio" id="envioPremium" name="tipo_de_envio" value="envioPremium">
+          <label for="envioPremium">Premium 2 a 5 días (15%)</label><br>
+        <input type="radio" id="envioExpress" name="tipo_de_envio" value="envioExpress">
+          <label for="envioExpress">Express 5 a 8 días (7%)</label><br>
+        <input type="radio" id="envioStandard" name="tipo_de_envio" value="envioStandard">
+          <label for="envioStandard">Standard 12 a 15 días (5%)</label>
+        <br>
+        <br>
 
       <h4>Dirección de envio</h4>
         <div class="row">
@@ -94,7 +97,38 @@ function renderizarCarrito() {
             <input type="text">
           </div>
         </div>
-        
+
+        <hr/>
+
+        <h4>Costos</h4>
+          <div class="row">
+            <div class="col">
+              <p>Subtotal</p>
+              <p><small>Costo unitario del producto por cantidad</small></p>
+            </div>
+            <div class="col">
+              <p>${valueInDollars}</p>
+            </div>
+          <div class="row">
+            <div class="col">
+              <p>Costo de envio</p>
+              <p><small>Según el tipo de envio</small></p>
+            </div>
+            <div class="col">
+              <p>$$$</p>
+            </div>
+          </div class="row">
+            <div>
+              <p>Total ($)</p>
+              <p>agregar precio total</p>
+            </div>
+          </div>
+        <hr/>
+
+        <h4>Forma de Pago</h4>
+          <p>No se ha seleccionado </p> <a>Seleccionar</a>
+
+        </div>
     `;
   document.getElementById("articulos").innerHTML = htmlContentToAppend;
 }
@@ -110,10 +144,17 @@ function actualizarPrecio() {
   const currentValue = document.getElementById("ammountValue").value;
   userData.articles[0].count = currentValue;
 
-  console.log("Hola");
   renderizarCarrito();
   document
     .getElementById("ammountValue")
     .setSelectionRange(currentValue.length, currentValue.length)
     .focus();
+}
+
+function actualizarPrecioEnvio() {
+  if (condition) {
+    
+  }
+
+  renderizarCarrito();
 }
