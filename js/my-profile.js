fbdc00d1b8cb03674b3986c2f569d7b2251c682e
larreborges <andresotero2@gmail.com>
2022-11-13 18:04:46 -0300

    Final

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,5 +1,3 @@
-let name = document.getElementById()
-
 function titular() {
     var cat = JSON.parse(localStorage.getItem('username'));
     document.getElementById("userName").innerHTML = cat;
@@ -49,10 +47,34 @@ function verifyName() {
 
 let button = document.getElementById("finalizarCompra");
 button.addEventListener("click", function () {
-  if (verifyName() && verifyLastname() && verifyEmail() && verifyEmail()) {
-    localStorage.setItem("name", );
-    localStorage.setItem("lastname", );
-    localStorage.setItem("email", );
-    localStorage.setItem("phone", )
+  let nameValue = document.getElementById("validationName").value
+  let lastnameValue = document.getElementById("validationLastName").value
+  let emailValue = document.getElementById("validationEmail").value
+  let phoneValue = document.getElementById("validationPhone").value
+  let nameValue2 = document.getElementById("noValidationName2").value
+  let lastnameValue2 = document.getElementById("noValidationLastName2").value
+
+  if (verifyName() && verifyLastname() && verifyEmail() && verifyPhone()) {
+    localStorage.setItem("name", nameValue);
+    localStorage.setItem("lastname", lastnameValue);
+    localStorage.setItem("email", emailValue);
+    localStorage.setItem("phone", phoneValue);
+    localStorage.setItem("name2", nameValue2);
+    localStorage.setItem("lastname2", lastnameValue2);
+    alert("Los datos se han guardado");
   }
 });
+
+function getData() {
+  let nameData = localStorage.getItem('name');
+  let lastnameData = localStorage.getItem('lastname');
+  let emailData = localStorage.getItem('email');
+  let phoneData = localStorage.getItem('phone');
+
+  document.getElementById("validationName").value = nameData;
+  document.getElementById("validationLastName").value = lastnameData;
+  document.getElementById("validationEmail").value = emailData;
+  document.getElementById("validationPhone").value = phoneData;
+}
+
+getData();
